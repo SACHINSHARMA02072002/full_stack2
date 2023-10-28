eb5d20558f313f6229603db721caaca0dde1b94e
Sachin sharma <sachinsharma02072002@gmail.com>
2023-10-28 13:59:01 +0530

    day_27

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import './index.css';
 import App from './App';
 import Wel from './wel';
 import Footer from './Footer'
--- src/wel.js
+++ src/wel.js
@@ -47,8 +47,8 @@ function Wel(){
   {/* Navbar */}
   {/* Hero */}
   <div className="p-5 text-center bg-light">
-    <h1 className="mb-3">Heading</h1>
-    <h4 className="mb-3">Subheading</h4>
+    <h1 className="mb-3">Hello , I am Sachin Sharma</h1>
+    <h4 className="mb-3">Let's Shake hand with me for wonderful project</h4>
     <a className="btn btn-primary" href="" role="button">
       Call to action
     </a>
